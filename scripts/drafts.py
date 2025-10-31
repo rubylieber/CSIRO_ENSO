b5536ac00c8e3ae3ec7f7ef945569d6dcf6f6b2f
Paul Gregory <pag548@gadi-login-03.gadi.nci.org.au>
2025-10-31 11:05:02 +1100

    UPDATES : New script to build lagged ens data

--- scripts/drafts.py
+++ scripts/drafts.py
@@ -6,43 +6,57 @@ import pytest
 import sys
 from pathlib import Path
 
-sys.path.append(Path(__file__).parent.resolve())
-
+sys.path.append('/home/548/pag548/code/CSIRO_ENSO/src')
+sys.path.append('/home/548/pag548/code/CSIRO_ENSO/config')
 from config import *
 from src import logger
 
-from src.load_data import generate_lag_dates, generate_file_list, generate_lag_dates_year
+from src.load_data import compute_hindcast_data
 
 LOG = logger.get_logger(__name__)
 
 month_day_string='0901'
 ens='e01'
 
-year='2000'
+data = compute_hindcast_data(2000,'0901')
 
-dates = generate_lag_dates_year(month_day_string,year)
+# A dictionary to hold a datarray of SON ensemble data for every year
+year_dict = {}
+
+year=2000
+
+dates = generate_lag_dates_year(month_day_string,str(year))
 ens_list = ['e01','e02','e03']
-a=list(itertools.product(ens_list,dates[::-1]))
+dates_ens_list=list(itertools.product(ens_list,dates[::-1]))
 
-patterns = [f"{a[0]}/maq5_pr_{a[1].strftime('%Y%m%d')}_{a[0]}.nc" for a in a ]
+#Add the ensemble number
+lagged_ens_list = [ d+(i+1,) for i,d in enumerate(dates_ens_list) ]
 
-matched = list(
-    itertools.chain.from_iterable(
-        CALIBRATED_PR_DIR.glob(pattern) for pattern in patterns
-    )
-)
 
+# A list of data for every lagged ensemble member
 data = []
 
-for i,filename in enumerate(matched[:5]):
-    ds = xr.open_dataset(filename).pr
+for ens in lagged_ens_list:
+    
+    # Construct file pattern for this
+    pattern = f"{ens[0]}/maq5_pr_{ens[1].strftime('%Y%m%d')}_{ens[0]}.nc"
 
+    # Find a file matching this pattern
+    filename = list(CALIBRATED_PR_DIR.glob(pattern))
 
-    # Follow this example
+    if filename: # Has the glob found a matching file in this directory
+        ds = xr.open_dataset(filename[0]).pr
+        LOG.info(f'Opening {filename[0].name}')
+    else:
+        LOG.error(f'No file matching {pattern} found in {CALIBRATED_PIR_DIR}')
+        LOG.error(f'Check definition of start dates and source directories')
+        sys.exit()
+
+    # Follow this example to compute a seasonal mean
     # https://docs.xarray.dev/en/stable/examples/monthly-means.html#
 
     month_length = ds.time.dt.days_in_month
-    year = ds.time.dt.year[0] #The year when the forecast is valid from
+
     weights = (
         month_length.groupby("time.season") / month_length.groupby("time.season").sum()
     )
@@ -51,10 +65,12 @@ for i,filename in enumerate(matched[:5]):
     ds_season = (ds * weights).groupby("time.season").sum(dim="time").sel(season='SON')
 
     # Assign the year to the seasonal dataset
-    #ds_season = ds_season.expand_dims("year").assign_coords(year=year)
+    #ds_season = ds_season.expand_dims("year").assign_coords(year=year.data)
 
     # Assign the ensemble number to the seasonal data
-    ds_season = ds_season.expand_dims("ens").assign_coords(ens=i+1)
+    ds_season = ds_season.expand_dims(["year","ens"]).assign_coords(year=[year],ens=[ens[-1]])
 
     data.append(ds_season)
 
+# Now concatenate all ensembles for this year
+year_dict[year] = xr.concat(data,dim='ens')
--- src/load_data.py
+++ src/load_data.py
@@ -8,12 +8,14 @@ import itertools
 import os
 import sys
 
-sys.path.append(Path(__file__).parent.resolve())
+SRC_DIR=Path(__file__).parent.resolve()
+PROJECT_ROOT_DIR = SRC_DIR.parent.resolve()
 
-from config import *
-from . import logger
+sys.path.insert(0, str(PROJECT_ROOT_DIR / 'config') )
+sys.path.insert(0, str(SRC_DIR ))
 
-CALIBRATED_PR_DIR = Path('/g/data/ux62/access-s2/hindcast/calibrated/atmos/pr/monthly/')
+from config import ENS_LIST, CALIBRATED_PR_DIR, YEAR_RANGE
+import logger
 
 LOG = logger.get_logger(__name__)
 
@@ -23,7 +25,6 @@ def generate_lag_dates_year(month_day_string,year):
     generate a list of timestamp objects across the entire hindcast period 1981-2018 for the nine-day lagged ensemble valid from this start date
     """
 
- 
     dates = []
 
     start_date = dt.datetime.strptime(str(year)+month_day_string,'%Y%m%d')
@@ -35,68 +36,116 @@ def generate_lag_dates_year(month_day_string,year):
     return dates
 
 
-def generate_lag_dates(month_day_string):
+def generate_file_list(month_day_string, 
+                       year):
     """
-    For a given start date expressed as a month-day-string (e.g. 0901), 
-    generate a list of timestamp objects across the entire hindcast period 1981-2018 for the nine-day lagged ensemble valid from this start date
-    """
-
-    years = range(1981,2019)
-
-    dates = []
-    for year in years:
-        start_date = dt.datetime.strptime(str(year)+month_day_string,'%Y%m%d')
-
-        date_range = pd.date_range(end=start_date,periods=9)
-
-        dates.extend(date_range)
-
-    return dates
-
-
-def generate_file_list(ens,dates):
-    """
-    Generate a list of ACCESS-S2 hindcast filepaths for a give ensemble string 
-    (e.g. 'e01') and list of dates
+    Generate a list of tuples containing the ensemble member, valid timesmap and index (1-27) for a ACCESS-S2 hindcast valid at the prescribed year 
+    (e.g. '2010') and month-day-string (e.g. '0901')
     """
 
-    allowed_ens = {'e01', 'e02', 'e03'} #Allowed values of ensemble string
+    dates = generate_lag_dates_year(month_day_string,
+                                    str(year)) 
+    dates_ens_list=list(itertools.product(ENS_LIST,dates[::-1]))
+
+    #Add the ensemble number to this data sturcture
+    lagged_ens_list = [ d+(i+1,) for i,d in enumerate(dates_ens_list) ]
+
+    return lagged_ens_list
 
 
-    if ens not in allowed_ens:
-        LOG.error(f'{ens} is not an allowed value : {allowed_ens}')
-        LOG.error('Specify an allowed ensemble string')
-        sys.exit()
-
-    patterns = [f"{ens}/maq5_pr_{dt.datetime.strftime(date,'%Y%m%d')}_{ens}.nc" for date in dates]
-
-    matched = list(
-        itertools.chain.from_iterable(
-            CALIBRATED_PR_DIR.glob(pattern) for pattern in patterns
-        )
-    )
-
-    return matched
-
-
-def load_data(ens,month_day_string):
+def seasonal_mean(ds):
     """
-    Load a complete ACCESS-S2 hindcast of precipiation valid from a specified
-    day of the month 'MMDD'
+    Compute a weighted seasonal mean (valid for September-October-November) of a dataset, following the example provided in the xarray docs
+    https://docs.xarray.dev/en/stable/examples/monthly-means.html
     """
-
-    dates = generate_lag_dates(month_day_string)
-
-    files = generate_file_list(ens,dates)
-
-    datasets = [ xr.open_dataset(file).pr for file in files[:9]]
-
-
-def load_dataset(filename):
-    """
-    Load a single ACCESS-S2 precip dataset into an xarray DataArray, and create
-    and ensemble dimensions based on the input filename.
-    """
-    data = xr.open_dataset(filename).pr
-
     
+    month_length = ds.time.dt.days_in_month
+
+    weights = (
+            month_length.groupby("time.season") / month_length.groupby("time.season").sum()
+        )
+
+    # Always select the Sep-Oct-Nov season
+    ds_season = (ds * weights).groupby("time.season").sum(dim="time").sel(season='SON')
+        
+    return ds_season
+
+
+def load_yearly_lagged_ensemble(month_day_string, 
+                                year):
+    """
+    Load a full 27-member ensemble ACCESS-S2 hindcast a given year 
+    (e.g. '2010') and month-day-string (e.g. '0901'). The hindcast is 
+    valid for the September-October-November ('SON') season
+    """
+    # A list of dataarrays for every lagged ensemble hindcast
+    data = []
+    
+    # Create a list of tuples which contains the ensemble string, timestamp
+    # and and index (1-27) for every member of the lagged ensemble
+    lagged_ens_list = generate_file_list(month_day_string, 
+                                            year)
+
+    for ens in lagged_ens_list:
+        # Construct file pattern for this
+        pattern = f"{ens[0]}/maq5_pr_{ens[1].strftime('%Y%m%d')}_{ens[0]}.nc"
+                                            
+        # Find a single file matching this pattern
+        filename = list(CALIBRATED_PR_DIR.glob(pattern))
+
+        if filename: # Has the glob found a matching file in this directory
+            try:
+                ds = xr.open_dataset(filename[0]).pr
+                LOG.info(f'Opening {filename[0].name}')
+            except:
+                LOG.error(f'Cannot open {filename[0]}. Does it contain "pr" data?')
+                LOG.error(f'Check defintion of CALIBRATED_PR_DIR : {CALIBRATED_PR_DIR}')
+                sys.exit()
+        else:
+            LOG.error(f'No file matching {pattern} found in {CALIBRATED_PIR_DIR}')
+            LOG.error(f'Check definition of start dates and source directories')
+            sys.exit()
+
+        # Compute seasonal mean for September-October-November
+        ds_season = seasonal_mean(ds)
+
+        # Assign the year and ensemble number the seasonal dataarray
+        ds_season = ds_season.expand_dims(["year","ens"]).assign_coords(year=[year],ens=[ens[-1]])
+
+        # Append to the list of datararray
+        data.append(ds_season)
+
+    # Now concatenate every ensemble member to create a single dataarray 
+    ds_year = xr.concat(data,dim='ens')
+
+    return ds_year
+
+
+def compute_hindcast_data(month_day_string):                                
+    """
+    Return a data array of dimension (38, 691, 886, 27))
+    38 years
+    691 latitude points
+    886 longitude points
+    27 ensemble members
+    of ACCESS-S2 September-October-November seasonal precipitation hindcasts,
+    valid from 'month_day_string'
+    """
+    
+    # A list of dataframes for every year of the hindcast period
+    data = []
+
+    for year in YEAR_RANGE:
+
+        # Load the 27-member lagged seasonal ensemble valid for this year
+        ds = load_yearly_lagged_ensemble(month_day_string, 
+                                    year)
+        LOG.info(f'Computed a seasonal precipitation hindcast for {year} with dimensions {ds.shape}')
+
+        # Append to the list of dataarrays
+        data.append(ds)
+
+    # Now concatenate every year data array to create a single dataarray
+    ds_hindcast = xr.concat(data,dim='year')
+
+    return ds_hindcast
